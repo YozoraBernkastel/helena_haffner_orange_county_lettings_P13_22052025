18397ada6f963577b5e6e259b2f1ddff65504aa9
Featherine <helaena.haffner@gmail.com>
2025-06-19 16:50:16 +0200

    #04 Unit tests for index and profiles done

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,1 +1,57 @@
-# Create your tests here.
+from django.test import Client
+from django.urls import reverse, resolve
+import pytest
+from pytest_django.asserts import assertTemplateUsed
+from django.contrib.auth.models import User
+from profiles.models import Profile
+
+
+def test_profile_index_url():
+    path = reverse('profiles_index', )
+    assert path == "/profiles/"
+    assert resolve(path).view_name == "profiles_index"
+
+
+@pytest.mark.django_db
+def test_profile_index_view():
+    client = Client()
+    response = client.get('/profiles/')
+    content = response.content.decode()
+    expected = "Profiles</h1>"
+
+    assert response.status_code == 200
+    assert expected in content
+    assertTemplateUsed(response, "profiles/index.html")
+
+
+def create_profile() -> Profile:
+    user = User.objects.create(username="Username", first_name="Prénom",
+                               last_name="Nom de Famille", email="test@mail.test")
+    return Profile.objects.create(user=user, favorite_city="TestCity")
+
+
+def init_profile() -> tuple:
+    client = Client()
+    profile = create_profile()
+    path = reverse("profile", kwargs={"username": profile.user.username})
+
+    return client, path
+
+
+@pytest.mark.django_db
+def test_profile_url():
+    client, path = init_profile()
+    assert path == "/profiles/Username/"
+    assert resolve(path).view_name == "profile"
+
+
+@pytest.mark.django_db
+def test_profile_view():
+    client, path = init_profile()
+    response = client.get(path)
+    content = response.content.decode()
+    expected = "<p><strong>First name :</strong> Prénom</p>"
+
+    assert response.status_code == 200
+    assert expected in content
+    assertTemplateUsed(response, "profiles/profile.html")
