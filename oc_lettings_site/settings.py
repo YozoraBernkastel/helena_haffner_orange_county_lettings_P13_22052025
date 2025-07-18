4899b2208ab5f9b79111818539114819676a705c
Featherine <helaena.haffner@gmail.com>
2025-07-18 13:45:54 +0200

    test

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -24,7 +24,6 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG_MODE") if os.environ.get("DEBUG_MODE") else False
 
-
 ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")] if os.environ.get("ALLOWED_HOSTS") else []
 
 
