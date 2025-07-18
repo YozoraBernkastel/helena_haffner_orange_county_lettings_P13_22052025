826e083a11da9e7c0f9238c1865a37d6e6458f7d
Featherine <helaena.haffner@gmail.com>
2025-07-18 10:57:59 +0200

    auto deploy

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -131,7 +131,7 @@ STORAGES = {
     },
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') if DEBUG else BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static", ]
